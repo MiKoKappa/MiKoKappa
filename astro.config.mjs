e6e2cb2e863eab2896d3189ff5d7ba15bea11751
Mikolaj Tkaczyk <mikolaj.tkaczyk@orange.com>
2026-03-20 10:51:59 +0100

    add deployment configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    output: "static",
+    site: 'https://tkaczykm.pl',
     integrations: [mdx(), sitemap(), tailwind(), react()],
 });
